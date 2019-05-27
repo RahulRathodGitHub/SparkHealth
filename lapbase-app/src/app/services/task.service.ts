a33b4daec96407a4354a03bda59bddac530166d0
Rahul Rathod <rahulrathod7399@gmail.com>
2019-05-27 22:59:26 +1000

    Task services created

--- lapbase-app/src/app/services/task.service.ts
+++ lapbase-app/src/app/services/task.service.ts
@@ -11,19 +11,19 @@ export class TaskService {
     return [
       {
         title: "Enter Data",
-        description: "gFASBFfwfrgreavegre,awrgaegeqrgeagegregeagrgwrfagreg,wrgaegargaer,ergr",
+        description: "This task is for entering your calories Intake",
         dueDate: "2019-07-17",
         "isCompleted": false
       },
       {
         title: "Enter Data nicely",
-        description: "gFASafawfaff__________argrgre,awrgaegeqrgeagegregeagrgwrfag",
+        description: "This task is for entering your Calories Burned",
         dueDate: "2018-04-17",
         "isCompleted": false
       },
       {
         title: "Enter Data nicely",
-        description: "gFASafawfaff__________argrgre,awrgaegeqrgeagegregeagrgwrfag",
+        description: "Just another Task",
         dueDate: "2019-05-17",
         "isCompleted": true
       }
