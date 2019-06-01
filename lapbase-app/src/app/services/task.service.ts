6b0f3782c5e90e6bf8742bb62f3265fe4eb4ff24
Rahul Rathod <rahulrathod7399@gmail.com>
2019-06-01 15:11:11 +1000

    Modified Task Component

--- lapbase-app/src/app/services/task.service.ts
+++ lapbase-app/src/app/services/task.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
 import { ITask } from '../models';
+import { environment } from './../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+  }
 
+  /*getTasks = async(): Promise<ITask[]> => {
+    return await this.httpClient.get(environment.LAPBASE_API_ADDRESS + 'Task').toPromise<any>();
+    }
+    
+    getTask = async(id: String): Promise<ITask> => {
+    return await this.httpClient.get(environment.LAPBASE_API_ADDRESS + 'Task/' + id).toPromise<any>();
+    }
+    */
   getTasks(): ITask[] {
     return [
       {
