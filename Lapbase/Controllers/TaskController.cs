fb2ac16dcda8190a44d8dedcf6f73822bb78dfee
Ammar Dayoub <ammardayoub6@gmail.com>
2019-06-01 15:49:40 +1000

    TEAM-96 Created TaskController and TaskService placeholders

---
+++ Lapbase/Controllers/TaskController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Lapbase.Models;
+using Lapbase.Services;
+
+namespace Lapbase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : Controller
+    {
+        private readonly TaskService taskService;
+
+        public TaskController(TaskService taskService)
+        {
+            this.taskService = taskService;
+        }
+    }
+}
---
+++ Lapbase/Services/TaskService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+using Lapbase.Models;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Lapbase.Services
+{
+    public class TaskService
+    {
+        private readonly LapbaseContext lapbaseContext;
+        private readonly IConfiguration config;
+
+        public TaskService(
+            IConfiguration config,
+            LapbaseContext lapbaseContext)
+        {
+            this.lapbaseContext = lapbaseContext;
+            this.config = config;
+        }
+    }
+}
