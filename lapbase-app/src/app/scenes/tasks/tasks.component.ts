4b2ef1815640af3d2a1961af598f3e8da2512f81
Ammar Dayoub <ammardayoub6@gmail.com>
2019-06-01 21:06:31 +1000

    Reverted back to using dummy data for the task service

--- lapbase-app/src/app/scenes/tasks/tasks.component.ts
+++ lapbase-app/src/app/scenes/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { TaskService } from './../../services/task.service';
 import { Component, OnInit } from '@angular/core';
 import { ITask } from 'src/app/models';
+import { Services } from '@angular/core/src/view';
 
 @Component({
   selector: 'app-tasks',
@@ -11,7 +12,8 @@ export class TasksComponent implements OnInit {
   private tasks: ITask[];
 
   constructor(service: TaskService) {
-    service.getTasks().then(result => this.tasks = result);
+    // service.getTasks().then(result => this.tasks = result);
+    this.tasks = service.getTasks();
   }
 
   ngOnInit() {
