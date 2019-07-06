304c373b10143cc9237702b5013e5bfb9cb8674e
Jianqiao Zhang <bridgez717@gmail.com>
2019-07-06 21:23:17 +1000

    Add more charts in reports

--- lapbase-app/src/app/scenes/reports/reports.component.ts
+++ lapbase-app/src/app/scenes/reports/reports.component.ts
@@ -1,28 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 @Component({
-  selector: 'app-reports',
-  templateUrl: './reports.component.html',
-  styleUrls: ['./reports.component.scss']
+  selector: "app-reports",
+  templateUrl: "./reports.component.html",
+  styleUrls: ["./reports.component.scss"]
 })
 export class ReportsComponent implements OnInit {
+  chartType = "";
+  weightLossData = [{}];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
   chartOptions = {
     responsive: true
   };
 
-  chartData = [
-    { data: [330, 600, 260, 700], label: 'Weight Loss' },
-    { data: [120, 455, 100, 340], label: 'BMI' },
-    { data: [45, 67, 800, 500], label: 'Calories intake' }
-  ];
+  chartDatas = {
+    weightLoss: { data: [330, 600, 260, 700], label: "Weight Loss" },
+    bmi: { data: [120, 455, 100, 340], label: "BMI" },
+    adverseEventSummary: {
+      data: [45, 67, 800, 500],
+      label: "Adverse Event Summary"
+    },
+    operationDetails: { data: [45, 67, 800, 500], label: "Operation Details" },
+    progress: { data: [45, 67, 800, 500], label: "Progress" }
+  };
 
-  chartLabels = ['January', 'February', 'Mars', 'April'];
+  chartData = [{ data: [330, 600, 260, 700], label: "Weight Loss" }];
+  chartLabels = ["January", "February", "Mars", "April"];
 
   onChartClick(event) {
     console.log(event);
+  }
+
+  setChartType(chartType) {
+    this.chartType = chartType;
+    this.chartData = [];
+    this.chartData.push(this.chartDatas[this.chartType]);
   }
 }
