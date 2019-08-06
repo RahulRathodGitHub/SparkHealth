f8836be5fc6a56662afa6cbc467da5b4b7407262
Rahul Rathod <rahulrathod7399@gmail.com>
2019-08-06 23:37:42 +1000

    The commit involves:
    - Incorporation of Full Calendar
    - A couple of plug-ins to make it look good

--- lapbase-app/src/app/scenes/appointments/appointments.component.ts
+++ lapbase-app/src/app/scenes/appointments/appointments.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGrigPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction'; // for dateClick
 
 @Component({
   selector: 'app-appointments',
   templateUrl: './appointments.component.html',
   styleUrls: ['./appointments.component.scss']
 })
-export class AppointmentsComponent implements OnInit {
+export class AppointmentsComponent{
 
-  constructor() { }
+  //The plugins can be added if we want more calendar functionality
+  calendarPlugins = [dayGridPlugin,      //For normal Day Grid Calendar View
+                     timeGrigPlugin, 
+                     interactionPlugin
+                    ];
 
-  ngOnInit() {
+  calendarEvents = [
+    {title: 'Event 1', date: '2019-08-01'},
+    {title: 'Event 2', date: '2019-08-02'}
+  ];
+
+  handleDateClick(event){
+    alert("You have an appointment on "+ event.event.start);
   }
+
+ /*addEvent() {
+    this.calendarEvents = this.calendarEvents.concat({ // creates a new array!
+      { title: 'Event 3', date: '2019-04-02' }
+    });
+  }*/
 
 }
