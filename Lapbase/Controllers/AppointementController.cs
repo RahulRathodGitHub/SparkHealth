83dac67b873368af5fcf572198fdeb06c7d6756d
Rahul Rathod <rahulrathod7399@gmail.com>
2019-08-10 16:05:18 +1000

    TEAMD-76 Backend Appointment Services and Controllers Created

---
+++ Lapbase/Controllers/AppointementController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lapbase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointementController : Controller
+    {
+        // GET: api/Appointement
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Appointement/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Appointement
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+    }
+}
---
+++ Lapbase/Services/AppointmentService.cs
@@ -0,0 +1,32 @@
+﻿using Lapbase.LapbaseModels;
+using Lapbase.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lapbase.Services
+{
+    public class AppointmentService
+    {
+        private readonly LapbaseContext lapbaseContext;
+        private readonly LapbaseNewContext lapbaseNewContext;
+        private readonly IConfiguration config;
+
+        public AppointmentService(
+            IConfiguration config,
+            LapbaseContext lapbaseContext,
+            LapbaseNewContext lapbaseNewContext)
+        {
+            this.lapbaseContext = lapbaseContext;
+            this.lapbaseNewContext = lapbaseNewContext;
+            this.config = config;
+        }
+
+        public async Task<List<Appointment>> GetAppointments()
+        {
+            return await lapbaseContext.Patient.ToListAsync();
+        }
+    }
+}
