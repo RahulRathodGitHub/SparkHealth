cc0674330a04643c4454ce010c8bfa2d46c631ae
Rahul Rathod <rahulrathod7399@gmail.com>
2019-08-21 13:44:03 +1000

    The Location is now available as per the Client's advised Table
    - Minor code clean up

--- Lapbase/Services/AppointmentService.cs
+++ Lapbase/Services/AppointmentService.cs
@@ -28,28 +28,23 @@ namespace Lapbase.Services
         public async Task<List<Appointment>> GetAppointments()
         {
             return ToAppointment(await lapbaseContext.TblPatientConsult.ToListAsync(), 
-                                 await lapbaseContext.TblDoctors.ToListAsync(), 
-                                 await lapbaseContext.TblHospitals.ToListAsync());
+                                 await lapbaseContext.TblDoctors.ToListAsync());
         }
 
         public async Task<List<Appointment>> GetAppointmentById(int id)
         {
             return ToAppointment(await lapbaseContext.TblPatientConsult.Where(p => p.PatientId == id).ToListAsync(),
-                                 await lapbaseContext.TblDoctors.ToListAsync(), 
-                                 await lapbaseContext.TblHospitals.ToListAsync());
+                                 await lapbaseContext.TblDoctors.ToListAsync());
         }
 
-        public List<Appointment> ToAppointment(List<TblPatientConsult> PatientConsults,
-                                               List<TblDoctors> Doctors,
-                                               List<TblHospitals> Hospitals)
+        public List<Appointment> ToAppointment(List<TblPatientConsult> PatientConsults, List<TblDoctors> Doctors)
         {
+            PatientConsults.OrderBy(t => t.DateSeen);
+
             var query =  from consult in PatientConsults
                          join doctor in Doctors
                          on consult.Seenby equals doctor.DoctorId into docPatientConsult // I want to add another condition for matching Organisation Code
                          from dpconsult in docPatientConsult.DefaultIfEmpty()
-                         join hospital in Hospitals
-                         on dpconsult.OrganizationCode equals hospital.OrganizationCode into mergedConsult
-                         from mconsult in mergedConsult.DefaultIfEmpty()
                          select new Appointment
                          {
                              Id = consult.ConsultId,
@@ -59,10 +54,9 @@ namespace Lapbase.Services
                              End = consult.DateSeen,
                              Description = consult.Notes,// == null ? "No Notes were provided" : consult.Notes,
                              DoctorName = dpconsult.DoctorName,
-                             Location = mconsult.HospitalName
+                             Location = dpconsult.Address1+", "+dpconsult.Suburb+", "+dpconsult.Country
                          };
 
-            //List<TblPatientConsult> patientConsults = PatientConsults.OrderBy(TblPatientConsult[0]);
             List<Appointment> appointments = query.ToList();
 
             //Below logic needs to be cleaned
@@ -77,79 +71,5 @@ namespace Lapbase.Services
             return appointments;
         }
 
-        /*public List<Appointment> ToAppointment2(List<TblPatientConsult> PatientConsults, 
-                                               List<TblDoctors> Doctors, 
-                                               List<TblOrganization> Organisations)
-        {
-            //The join has a problem.
-            var query =  from consult in PatientConsults
-                         join doctor in Doctors
-                         on consult.Seenby equals doctor.DoctorId into docPatientConsult
-                         from dpconsult in docPatientConsult.DefaultIfEmpty(null)
-                         join organisation in Organisations 
-                         on dpconsult.OrganizationCode equals organisation.Code into mergedConsult
-                         from mconsult in mergedConsult.DefaultIfEmpty(null)
-                         select new Appointment
-                         {
-                            Id = consult.ConsultId,
-                            Title = "Appointment",
-                            PatientId = consult.PatientId,
-                            Start = consult.DateSeen,
-                            End = consult.DateSeen,
-                            Description = consult.Notes,
-                            DoctorName = dpconsult.DoctorName,
-                            Location = mconsult.OrgDomainName
-                        };
-
-            var query2 = from consult in PatientConsults
-                        select new Appointment
-                        {
-                            Id = consult.ConsultId,
-                            Title = "Appointment",
-                            PatientId = consult.PatientId,
-                            Start = consult.DateSeen,
-                            End = consult.DateSeen,
-                            Description = consult.Notes,
-                            DoctorName = "Doctor hahahha",
-                            Location = "hahahahahahahahah"
-                        };
-
-
-
-            //for the next Visit just extract the last entry copy it and modify the start to the next date. then add it to the list.
-            //List<TblPatientConsult> patientConsults = PatientConsults.OrderBy(TblPatientConsult[0]);
-            List<Appointment> appointments = new List<Appointment>();
-
-            foreach (TblPatientConsult consult in PatientConsults)
-            {
-
-                Appointment appointment = new Appointment();
-                appointment.Id = consult.ConsultId;
-                appointment.Title = "Appointment";
-                appointment.PatientId = consult.PatientId;
-                appointment.Start = consult.DateSeen;
-                appointment.End = consult.DateSeen;
-                appointment.Description = consult.Notes;
-
-                appointments.Add(appointment);
-
-            }
-
-            TblPatientConsult lastConsult = PatientConsults.Last();
-            Appointment futureAppointment = new Appointment();
-            futureAppointment.Id = lastConsult.ConsultId;
-            futureAppointment.Title = "Appointment";
-            futureAppointment.PatientId = lastConsult.PatientId;
-            futureAppointment.Start = lastConsult.DateNextVisit;
-            futureAppointment.End = lastConsult.DateNextVisit;
-            futureAppointment.Description = lastConsult.Notes;
-
-
-            appointments.Add(futureAppointment);
-
-            return appointments;
-
-        }
-        */
     }
 }
