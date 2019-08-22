fb3eaa15aa4e9549d497ae0a005e48400a4ffa74
Ammar Dayoub <ammar.dayoub@ipcompute.com>
2019-08-22 13:25:11 +1000

    TEAMD-83 Fixed appointment list query, Fixed some tslint warnings

--- lapbase-app/src/app/scenes/appointments/appointments.component.ts
+++ lapbase-app/src/app/scenes/appointments/appointments.component.ts
@@ -3,7 +3,6 @@ import { Component, ViewChild } from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGrigPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction'; // for dateClick
-import CalendarComponent from '@fullcalendar/core/CalendarComponent';
 import { FullCalendarComponent } from '@fullcalendar/angular';
 import { IAppointment } from 'src/app/models';
 
@@ -13,64 +12,50 @@ import { IAppointment } from 'src/app/models';
   templateUrl: './appointments.component.html',
   styleUrls: ['./appointments.component.scss']
 })
-export class AppointmentsComponent{
+export class AppointmentsComponent {
 
   calendarTitle;
   calendarEvents: IAppointment[];
 
   eventClicked = false;
-  eventTitle = "Empty Event";
-  eventDescription = "";
-  eventDoctorName = "";
-  eventLocation = "";
-  eventTime = "";
+  eventTitle = 'Empty Event';
+  eventDescription = '';
+  eventDoctorName = '';
+  eventLocation = '';
+  eventTime = '';
 
-  constructor(private appointmentService: AppointmentService){
-    //this.calendarEvents = this.appointmentService.getAppointmentsDemo();
-    //this.appointmentService.getAppointments().then(result => this.calendarEvents = result);
+  constructor(private appointmentService: AppointmentService) {
+    // this.calendarEvents = this.appointmentService.getAppointmentsDemo();
+    // this.appointmentService.getAppointments().then(result => this.calendarEvents = result);
     this.appointmentService.getAppointmentsById(107057612).then(result => this.calendarEvents = result);
-    //107068092 -> Another good patient ID with a description.
-    //The description is provided in the notes part of the DB; especially deleted by wayne
-    //console.log(this.calendarEvents);
+    // 107068092 -> Another good patient ID with a description.
+    // The description is provided in the notes part of the DB; especially deleted by wayne
+    // console.log(this.calendarEvents);
   }
 
   @ViewChild('calendar') calendarComponent: FullCalendarComponent;
 
-  getCalendarTitle(){
-
-    let calendarApi = this.calendarComponent.getApi();
-    //Using this API I can recreate the design using Bulma
-
-    if(calendarApi) return calendarApi.view.title;
-    else return "";
-  }
-
-  //The plugins can be added if we want more calendar functionality
-  calendarPlugins = [dayGridPlugin,      //For normal Day Grid Calendar View
-                     timeGrigPlugin, 
-                     interactionPlugin
-                    ];
+  // The plugins can be added if we want more calendar functionality
+  calendarPlugins = [dayGridPlugin,      // For normal Day Grid Calendar View
+    timeGrigPlugin,
+    interactionPlugin
+  ];
 
 
-  handleDateClick(event){
-
+  handleDateClick(event) {
     this.eventClicked = true;
     this.eventTitle = event.event.title;
     this.eventDoctorName = event.event.extendedProps.doctorName;
     this.eventLocation = event.event.extendedProps.location;
     this.eventTime = event.event.start.toDateString();
 
-    if(event.event){
-      if(event.event.extendedProps.description.length != 0)//Implies that the doctor have written notes
-      {
-        this.eventDescription = event.event.extendedProps.description;   
-      }
-      else
-      {
-        this.eventDescription = "";
+    if (event.event) {
+      if (event.event.extendedProps.description.length !== 0) {
+          this.eventDescription = event.event.extendedProps.description;
+        } else {
+        this.eventDescription = '';
       }
     }
-  
   }
 
 }
