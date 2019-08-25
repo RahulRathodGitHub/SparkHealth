6b23696735604ac8c719f779a6f8262b7fd7b94e
Rahul Rathod <rahulrathod7399@gmail.com>
2019-08-25 17:07:53 +1000

    solved merge conflicts

--- lapbase-app/src/app/scenes/reports/reports.component.ts
+++ lapbase-app/src/app/scenes/reports/reports.component.ts
@@ -1,15 +1,10 @@
 import { Component, OnInit } from "@angular/core";
-<<<<<<< HEAD
 import { ACTIVE_INDEX } from "@angular/core/src/render3/interfaces/container";
 import { ReportService } from "src/app/services/report.service";
 import { reporttype, IReport } from "src/app/models/report";
 import { DatePipe } from "@angular/common";
-
-=======
-import { ACTIVE_INDEX } from '@angular/core/src/render3/interfaces/container';
 import {IMyDrpOptions} from 'mydaterangepicker';
 import {IMyDpOptions} from 'mydatepicker';
->>>>>>> origin/TEAMD-120_Date_Pick_In_Report_Page
 @Component({
   selector: "app-reports",
   templateUrl: "./reports.component.html",
@@ -24,10 +19,6 @@ export class ReportsComponent implements OnInit {
 
   dropdownActive = false;
   weightLossData = [{}];
-<<<<<<< HEAD
-=======
-                       constructor() {}
->>>>>>> origin/TEAMD-120_Date_Pick_In_Report_Page
 
   constructor(
     private datepipe: DatePipe,
@@ -60,7 +51,6 @@ export class ReportsComponent implements OnInit {
       // case "twl" : number = reporttype['Twl'];break;
       // case "progress" : number = reporttype['Progress'];break;
     }
-<<<<<<< HEAD
 
     //Retrieving weight data as default data when users visit the Report page
     var today = new Date();
@@ -109,7 +99,6 @@ export class ReportsComponent implements OnInit {
   public drop() {
     this.dropdownActive = !this.dropdownActive;
   }
-=======
     public startDateOptions: IMyDpOptions = {
       // other options...
       dateFormat: 'dd.mm.yyyy',
@@ -127,5 +116,4 @@ export class ReportsComponent implements OnInit {
   }
   private startDatePlaceholder: string = 'Start date';
   private endDatePlaceholder: string = 'End date';
->>>>>>> origin/TEAMD-120_Date_Pick_In_Report_Page
 }
