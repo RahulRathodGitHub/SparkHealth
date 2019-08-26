b72af954e91fe57394d58891d77e360ef15867a0
Ammar Dayoub <ammar.dayoub@ipcompute.com>
2019-08-26 11:49:04 +1000

    Updated user settings page to be empty like expected

--- lapbase-app/src/app/scenes/account/account.component.ts
+++ lapbase-app/src/app/scenes/account/account.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { PatientService } from 'src/app/services';
-import { IPatient } from 'src/app/models';
 
 @Component({
   selector: 'app-account',
@@ -8,20 +7,8 @@ import { IPatient } from 'src/app/models';
   styleUrls: ['./account.component.scss']
 })
 export class AccountComponent implements OnInit {
-  patients: IPatient[];
-  patient: IPatient;
-  patientsLapbase: string[];
 
   constructor(private patientService: PatientService) {
-    patientService.getPatients().then((result) =>
-      this.patients = result
-    );
-    patientService.getPatient(1).then((result) =>
-      this.patient = result
-    );
-    patientService.getPatientsLapbase().then((result) =>
-      this.patientsLapbase = result
-    );
   }
 
   ngOnInit() {
