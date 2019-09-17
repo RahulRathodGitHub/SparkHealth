8da13a86da5d42731aee7a13170760e9f44f5ba9
Jianqiao Zhang <bridgez717@gmail.com>
2019-09-17 14:26:17 +1000

    Change the task page to our new data entry design.

--- lapbase-app/src/app/scenes/tasks/tasks.component.ts
+++ lapbase-app/src/app/scenes/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { TaskService } from './../../services/task.service';
 import { Component, OnInit } from '@angular/core';
 import { ITask, TaskType } from 'src/app/models';
 
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
@@ -13,9 +14,36 @@ export class TasksComponent implements OnInit {
   selectedTaskType: TaskType;
   step: number;
 
+//   data: {
+//     "id": "db225c97-8515-4087-aeb9-f519cea4edea",
+//     "patientId": 1,
+//     "organizationCode": 1,
+//     "dateAssigned": "2019-09-12T09:24:44.0948073+00:00",
+//     "calories": 3000.00,
+//     "weight": 70.00,
+//     "foods": [
+//                 {
+//                     "name": "Food1,Food 2,Food 3",
+//                     "quantity": "1,2,3",
+//                     "mealTime": "Breakfast"
+//                 },
+//                 {
+//                     "name": "Food1,Food 2,Food 3",
+//                     "quantity": "1,2,3",
+//                     "mealTime": "Lunch"
+//                 }
+//     ],
+//     "exercises": {
+//         "name": "push ups, Ex 2"
+//         "quantity": "10, 20"
+//     }
+// }
+
   constructor(private taskService: TaskService) {
     taskService.getTasks().then(result => this.tasks = result);
     this.step = 0;
+
+    
   }
 
   onButtonClick(taskId: string) {
@@ -36,5 +64,8 @@ export class TasksComponent implements OnInit {
   }
 
   ngOnInit() {
+   
   }
+
+  
 }
