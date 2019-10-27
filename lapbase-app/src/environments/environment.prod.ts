dba2551e5982b37e516a766bd26532e91ba95dac
Ammar Dayoub <ammar.dayoub@ipcompute.com>
2019-10-28 09:27:38 +1100

    Final commit for presentation

--- lapbase-app/src/environments/environment.prod.ts
+++ lapbase-app/src/environments/environment.prod.ts
@@ -6,5 +6,8 @@
   REDIRECT_URI: 'https://app-aatvrbmipgvkp.azurewebsites.net/Dashboard',
   POST_LOGOUT_REDIRECT_URI: 'https://app-aatvrbmipgvkp.azurewebsites.net',
   AUTHORITY: 'https://login.microsoftonline.com/2e833ca0-6a32-45e7-b968-1faae0218ea4',
-  CONSENT_SCOPES: ['api://b9e4a478-e93e-4bea-bb73-cf23d5bfefe0/user_impersonation']
+  CONSENT_SCOPES: ['api://b9e4a478-e93e-4bea-bb73-cf23d5bfefe0/user_impersonation'],
+    PROTECTED_RESOURCE_MAP: [
+    ['https://api-aatvrbmipgvkp.azurewebsites.net/api/', ['api://b9e4a478-e93e-4bea-bb73-cf23d5bfefe0/user_impersonation']]
+  ]
 };
--- lapbase-app/src/environments/environment.ts
+++ lapbase-app/src/environments/environment.ts
@@ -10,9 +10,9 @@ export const environment = {
   REDIRECT_URI: 'http://localhost:4200/Dashboard',
   POST_LOGOUT_REDIRECT_URI: 'http://localhost:4200',
   AUTHORITY: 'https://login.microsoftonline.com/2e833ca0-6a32-45e7-b968-1faae0218ea4',
-  CONSENT_SCOPES: ['api://b9e4a478-e93e-4bea-bb73-cf23d5bfefe0/.default"'],
+  // CONSENT_SCOPES: ['api://b9e4a478-e93e-4bea-bb73-cf23d5bfefe0/user_impersonation"'],
   PROTECTED_RESOURCE_MAP: [
-    ['https://localhost:5001/api', ['api://b9e4a478-e93e-4bea-bb73-cf23d5bfefe0/.default']]
+    ['https://localhost:5001/api', ['api://b9e4a478-e93e-4bea-bb73-cf23d5bfefe0/user_impersonation']]
   ]
 };
 
