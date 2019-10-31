2102fd4b679b64f944605222b90af07dfbbd9011
Ammar Dayoub <ammar.dayoub@ipcompute.com>
2019-10-31 11:30:06 +1100

    Refactored testing, Implemented testing for TaskInputService

--- Lapbase.Test/TaskInputServiceTest.cs
+++ Lapbase.Test/TaskInputServiceTest.cs
@@ -12,175 +12,177 @@ namespace Lapbase.Tests
     [TestFixture]
     public class TaskInputServiceTest
     {
-        DbContextOptionsBuilder<LapbaseNewContext> DbContextOptionsBuilder = new DbContextOptionsBuilder<LapbaseNewContext>();
-        DbContextOptions<LapbaseNewContext> options;
-        IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
-        IConfiguration config;
+        private readonly LapbaseNewContext context;
+        private readonly IConfiguration config;
+        private readonly DbContextOptions<LapbaseNewContext> options;
+        private Patient patient;
+
+        public TaskInputServiceTest()
+        {
+            var DbContextOptionsBuilder = new DbContextOptionsBuilder<LapbaseNewContext>();
+            options = DbContextOptionsBuilder
+                    .UseInMemoryDatabase(databaseName: "Test_Database")
+                    .Options;
+            var configurationBuilder = new ConfigurationBuilder();
+            config = configurationBuilder.Build();
+            context = new LapbaseNewContext(options);
+
+        }
 
         [SetUp]
-        public void Setup()
+        public void SetupPatient()
         {
-            options = DbContextOptionsBuilder
-                .UseInMemoryDatabase(databaseName: "Test_Database")
-                .Options;
+            patient = context.Patient.Add(new Patient
+            {
+                PatientCode = int.MaxValue,
+                OrganisationCode = int.MaxValue,
+                Username = "TestUser",
+                WhenCreated = DateTimeOffset.UtcNow,
+                ValidFrom = DateTimeOffset.UtcNow,
+                ValidTo = DateTimeOffset.UtcNow.AddDays(15),
+                Deleted = false
+            }).Entity;
+            context.SaveChanges();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            context.Database.EnsureDeleted();
         }
 
         [Test]
         public async Task GetTaskInputByDate_WithNonUsedDate_ReturnsNewTaskInputDto()
         {
-            using (var context = new LapbaseNewContext(options))
-            {
-                // Arrange
-                config = configurationBuilder.Build();
-                TaskInputService service = new TaskInputService(config, context);
+            // Arrange
+            TaskInputService service = new TaskInputService(config, context);
 
-                // Act
-                var result = await service.GetTaskInputByDate(new DateTimeOffset(), 2756, 1);
+            // Act
+            var result = await service.GetTaskInputByDate(new DateTimeOffset(), patient.PatientCode, patient.OrganisationCode);
 
-                // Assert
-                Assert.That(result.Id, Is.EqualTo(Guid.Empty));
-            }
+            // Assert
+            Assert.That(result.Id, Is.EqualTo(Guid.Empty));
         }
 
         [Test]
         public async Task GetTaskInputByDate_WithUsedDate_ReturnsExistingTaskInputDto()
         {
-            using (var context = new LapbaseNewContext(options))
+            // Arrange
+            TaskInputService service = new TaskInputService(config, context);
+            TaskInput taskInput = context.TaskInput.Add(new TaskInput
             {
-                // Arrange
-                config = configurationBuilder.Build();
-                TaskInputService service = new TaskInputService(config, context);
-                TaskInput taskInput = context.TaskInput.Add(new TaskInput
-                {
-                    Id = Guid.NewGuid(),
-                    PatientId = 2756,
-                    OrganizationCode = 1,
-                    DateAssigned = new DateTimeOffset(2019, 05, 05, 0, 0, 0, new TimeSpan())
-                }).Entity;
-                context.SaveChanges();
+                Id = Guid.NewGuid(),
+                PatientId = patient.PatientCode,
+                OrganizationCode = patient.OrganisationCode,
+                DateAssigned = new DateTimeOffset(2019, 05, 05, 0, 0, 0, new TimeSpan())
+            }).Entity;
+            context.SaveChanges();
 
-                // Act
-                var result = await service.GetTaskInputByDate(new DateTimeOffset(2019, 05, 05, 0, 0, 0, new TimeSpan()), 2756, 1);
+            // Act
+            var result = await service.GetTaskInputByDate(new DateTimeOffset(2019, 05, 05, 0, 0, 0, new TimeSpan()), patient.PatientCode, patient.OrganisationCode);
 
-                // Assert
-                Assert.That(result, Is.Not.Null);
-                Assert.That(result.Id, Is.EqualTo(taskInput.Id));
-            }
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Id, Is.EqualTo(taskInput.Id));
         }
 
         [Test]
         public async Task UpdateTaskInput_WithNewTaskInput_ReturnsNewTaskInput()
         {
-            using (var context = new LapbaseNewContext(options))
+            // Arrange
+            TaskInputService service = new TaskInputService(config, context);
+
+            // Act
+            var result = await service.UpdateTaskInput(new TaskInputDto
             {
-                // Arrange
-                config = configurationBuilder.Build();
-                TaskInputService service = new TaskInputService(config, context);
+                Id = Guid.NewGuid(),
+                Weight = 75,
+                DateAssigned = new DateTimeOffset(2019, 05, 05, 0, 0, 0, new TimeSpan())
+            }, patient.PatientCode, patient.OrganisationCode);
 
-                // Act
-                var result = await service.UpdateTaskInput(new TaskInputDto
-                {
-                    Id = Guid.NewGuid(),
-                    Weight = 75,
-                    DateAssigned = new DateTimeOffset(2019, 05, 05, 0, 0, 0, new TimeSpan())
-                }, 1, 1);
-
-                // Assert
-                Assert.That(result, Is.Not.Null);
-                Assert.That(result.Id, Is.Not.EqualTo(Guid.Empty));
-                Assert.That(result.Weight, Is.EqualTo(75));
-            }
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Id, Is.Not.EqualTo(Guid.Empty));
+            Assert.That(result.Weight, Is.EqualTo(75));
         }
 
         [Test]
         public async Task UpdateTaskInput_WithExitingTaskInput_ReturnsUpdatedTaskInput()
         {
-            using (var context = new LapbaseNewContext(options))
+            // Arrange
+            TaskInputService service = new TaskInputService(config, context);
+            TaskInput taskInput = context.TaskInput.Add(new TaskInput
             {
-                // Arrange
-                config = configurationBuilder.Build();
-                TaskInputService service = new TaskInputService(config, context);
-                TaskInput taskInput = context.TaskInput.Add(new TaskInput
-                {
-                    Id = Guid.NewGuid(),
-                    PatientId = 2756,
-                    OrganizationCode = 1,
-                    Weight = 60,
-                    DateAssigned = new DateTimeOffset(2019, 05, 05, 0, 0, 0, new TimeSpan())
-                }).Entity;
-                context.SaveChanges();
+                Id = Guid.NewGuid(),
+                PatientId = patient.PatientCode,
+                OrganizationCode = patient.OrganisationCode,
+                Weight = 60,
+                DateAssigned = new DateTimeOffset(2019, 05, 05, 0, 0, 0, new TimeSpan())
+            }).Entity;
+            context.SaveChanges();
 
-                // Act
-                var result = await service.UpdateTaskInput(new TaskInputDto
-                {
-                    Id = taskInput.Id,
-                    Weight = 75,
-                    DateAssigned = new DateTimeOffset(2019, 05, 05, 0, 0, 0, new TimeSpan())
-                }, 1, 1);
+            // Act
+            var result = await service.UpdateTaskInput(new TaskInputDto
+            {
+                Id = taskInput.Id,
+                Weight = 75,
+                DateAssigned = new DateTimeOffset(2019, 05, 05, 0, 0, 0, new TimeSpan())
+            }, patient.PatientCode, patient.OrganisationCode);
 
-                // Assert
-                Assert.That(result, Is.Not.Null);
-                Assert.That(result.Id, Is.EqualTo(taskInput.Id));
-                Assert.That(result.Weight, Is.EqualTo(75));
-            }
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Id, Is.EqualTo(taskInput.Id));
+            Assert.That(result.Weight, Is.EqualTo(75));
         }
 
         [Test]
         public void GetFoodOfTheMonth_WithNoTaskInputs_ReturnsEmptyGuid()
         {
-            using (var context = new LapbaseNewContext(options))
-            {
-                // Arrange
-                config = configurationBuilder.Build();
-                TaskInputService service = new TaskInputService(config, context);
+            // Arrange
+            TaskInputService service = new TaskInputService(config, context);
 
-                // Act
-                var result = service.GetFoodOfTheMonth(2756, 1);
+            // Act
+            var result = service.GetFoodOfTheMonth(patient.PatientCode, patient.OrganisationCode);
 
-                // Assert
-                Assert.That(result, Is.Not.Null);
-                Assert.That(result, Is.EqualTo(Guid.Empty));
-            }
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.EqualTo(Guid.Empty));
         }
 
         [Test]
         public void GetFoodOfTheMonth_WithExistingTaskInputs_ReturnsFoodGuid()
         {
-            using (var context = new LapbaseNewContext(options))
+            // Arrange
+            TaskInputService service = new TaskInputService(config, context);
+            var food = context.Food.Add(new Food
             {
-                // Arrange
-                config = configurationBuilder.Build();
-                TaskInputService service = new TaskInputService(config, context);
-                var food = context.Food.Add(new Food
-                {
-                    Id = Guid.NewGuid(),
-                    CalorieCount = 70,
-                    Name = "TestFood",
-                    Unit = "Piece"
-                }).Entity;
-                context.SaveChanges();
-                context.TaskInput.Add(new TaskInput
-                {
-                    Id = Guid.NewGuid(),
-                    PatientId = 2756,
-                    OrganizationCode = 1,
-                    Weight = 60,
-                    Foods = food.Id.ToString(),
-                    FoodQuantities = "3",
-                    MealTimes = "1",
-                    Exercises = "",
-                    ExerciseReps = "",
-                    DateAssigned = DateTimeOffset.UtcNow.AddDays(-10)
-                });
-                context.SaveChanges();
+                Id = Guid.NewGuid(),
+                CalorieCount = 70,
+                Name = "TestFood",
+                Unit = "Piece"
+            }).Entity;
+            context.SaveChanges();
+            context.TaskInput.Add(new TaskInput
+            {
+                Id = Guid.NewGuid(),
+                PatientId = patient.PatientCode,
+                OrganizationCode = patient.OrganisationCode,
+                Weight = 60,
+                Foods = food.Id.ToString(),
+                FoodQuantities = "3",
+                MealTimes = "1",
+                Exercises = "",
+                ExerciseReps = "",
+                DateAssigned = DateTimeOffset.UtcNow.AddDays(-10)
+            });
+            context.SaveChanges();
 
-                // Act
-                var result = service.GetFoodOfTheMonth(2756, 1);
+            // Act
+            var result = service.GetFoodOfTheMonth(patient.PatientCode, patient.OrganisationCode);
 
-                // Assert
-                Assert.That(result, Is.Not.Null);
-                Assert.That(result, Is.EqualTo(food.Id));
-            }
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.EqualTo(food.Id));
         }
     }
 }
